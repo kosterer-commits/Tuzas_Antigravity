bbb879147838951d6c42fa39f28abc7bad6e1d3d
kosterer-commits <koster.er@pachuca.tecnm.mx>
2026-02-16 11:29:27 -0600

    se arreglaron las rutas de las imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://kosterer-commits.github.io',
   base: '/Tuzas_Antigravity/',
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
